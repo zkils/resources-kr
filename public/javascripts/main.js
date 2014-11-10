5e80e0f91db051e80c7ef09678838d764da3d8ae
zkils <zkils8302@gmail.com>
2014-11-10 08:53:55 +0000

    casoursel Slide finish

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,22 +5,50 @@
 $(document).ready(function(){
     //$(".photo-slide.one").siblings().fadeOut();
     //animatePhotoSlide();
+    $(".carousel-indicator .btn").on("click",carouselSlide);
 });
 
-function animatePhotoSlide(){
-    $(".photo-slide.two").animate({
-        opacity: 1
-    },5000, function(){
-        $(".photo-slide.two").fadeOut();
-        $(".photo-slide.three").animate({
-            opacity: 1
-        },5000,function(){
-            $(".photo-slide.three").fadeOut();
-            $(".photo-slide.one").animate({
-                opacity: 1
-            },5000,function(){
-                $(".photo-slide.one").fadeOut();
-            });
+function carouselSlide(){
+    var $this = $(this);
+    var index = $this.index();
+    var $slide ;
+    if($this.hasClass("off")){
+        $slide = $(".carousel-slide");
+        if(index==0){
+            $slide.first().animate({
+                left:0,
+                duration:3000
+            },"easeInCubic")
+            $slide.last().animate({
+                left:2400,
+                duration:3000
+            },"easeInCubic")
+        }else{
+            $slide.first().animate({
+                left:-2400,
+                duration:3000
+            },"easeInCubic")
+            $slide.last().animate({
+                left:0,
+                duration:3000
+            },"easeInCubic")
+        }
+
+        /*
+        $slide.each(function(){
+            var $this = $(this);
+            if(index==0){
+                $this.
+            }else{
+
+            }
         });
-    });
+        */
+        //$slide = $(".carousel-slide").eq(index);
+        $this.removeClass("off").addClass("on");
+        $this.siblings().removeClass("on").addClass("off");
+
+
+
+    }
 }
